557fb31fae381837327eb12b81fcdf68141b6b4c
Afonso Matos <afonsolfmatos@gmail.com>
2018-11-07 18:39:34 +0000

    change collision matrix to 2d vector implementation

--- src/geobattle/collider/CollisionMatrix.java
+++ src/geobattle/collider/CollisionMatrix.java
@@ -1,35 +1,38 @@
 package geobattle.collider;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import geobattle.core.Tag;
-import geobattle.util.Log;
 
 public final class CollisionMatrix {
 	
-	// TODO: Longs have at most 64 bits, so the current implementation fails
-	// when there are more than 64 tags.
-	private Map<Integer, Long> matrix = new HashMap<Integer, Long>();
+	private final static int MAX_TAGS = 1000;
+	
+	private boolean[][] matrix = new boolean[MAX_TAGS][MAX_TAGS];
+	
+	private void verifyTags(Tag... tags) {
+		for (Tag t : tags)
+			if (t.id >= MAX_TAGS)
+				throw new IllegalArgumentException("Tag id must be < " + MAX_TAGS);
+	}
 	
 	public CollisionMatrix add(Tag tag1, Tag tag2) {
+		verifyTags(tag1, tag2);
 		int id1 = tag1.id;
 		int id2 = tag2.id;
-		matrix.put(id1, matrix.getOrDefault(id1, 0L) | (1 << id2));
-		matrix.put(id2, matrix.getOrDefault(id2, 0L) | (1 << id1));
+		matrix[id1][id2] = matrix[id2][id1] = true;
 		return this;
 	}
 	
 	public CollisionMatrix remove(Tag tag1, Tag tag2) {
+		verifyTags(tag1, tag2);
 		int id1 = tag1.id;
 		int id2 = tag2.id;
-		matrix.put(id1, matrix.getOrDefault(id1, 0L) & ~(1 << id2));
-		matrix.put(id2, matrix.getOrDefault(id2, 0L) & ~(1 << id1));	
+		matrix[id1][id2] = matrix[id2][id1] = false;
 		return this;
 	}
 	
 	public boolean collidesWith(Tag tag1, Tag tag2) {
-		return 0 != (matrix.getOrDefault(tag1.id, 0L) & (1 << tag2.id));
+		verifyTags(tag1, tag2);
+		return matrix[tag1.id][tag2.id];
 	}
 
 }
