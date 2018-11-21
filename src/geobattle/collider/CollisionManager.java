3aeaa74d05a38420b5073fc754054a2aea317f0f
Afonso Matos <afonsolfmatos@gmail.com>
2018-11-21 10:03:42 +0000

    Extra docs and cleanup of CollisionManager

--- src/geobattle/collider/CollisionManager.java
+++ src/geobattle/collider/CollisionManager.java
@@ -3,6 +3,8 @@ package geobattle.collider;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
@@ -35,44 +37,50 @@ public class CollisionManager {
 	/**
 	 * Traces all collisions of all game objects in the moment it is called, and
 	 * updates every game object's {@code Collider} with this information.
+	 * Colliders are responsible for handling collision events.
 	 * 
 	 * @see Collider
 	 * @see CollisionHandler
 	 */
 	public void handleCollisions() {
+		
+		// Get all active colliders
+		ArrayList<Collider> colliders =
+				game.getGameObjects()
+					.stream()
+					.filter(GameObject::isActive)
+					.map(GameObject::getCollider)
+					.filter(Objects::nonNull)
+					/*
+					 * Possible improvement:
+					 * () -> new ArrayList<>(gameObjects.size())
+					 */
+					.collect(Collectors.toCollection(ArrayList::new));
+		
+		Map<Collider, List<Collider>> collisions =
+				new HashMap<Collider, List<Collider>>();
 
-		// TODO Make this prettier
-
-		List<GameObject> gameObjects = game.getGameObjects();
-
-		List<Collider> colliders = gameObjects
-				.parallelStream()
-				.map(GameObject::getCollider)
-				.filter(Objects::nonNull)
-				.collect(Collectors.toCollection(
-						() -> new ArrayList<>(gameObjects.size())));
-
-		HashMap<Collider, List<Collider>> collisions = new HashMap<Collider, List<Collider>>();
-
+		// Set a new list for all colliders
+		colliders.forEach(c -> collisions.put(c, new ArrayList<>()));
+		
+		// Loop through 
 		int size = colliders.size();
 		for (int i = 0; i < size; ++i) {
 			Collider c1 = colliders.get(i);
-			collisions.putIfAbsent(c1, new ArrayList<Collider>());
-
-			for (int j = i + 1; j < size; j++) {
+			for (int j = i + 1; j < size; ++j) {
 				Collider c2 = colliders.get(j);
+				/*
+				 * Check whether the two colliders are meant to collide and if
+				 * their collision box clash.
+				 */
 				if (collisionMatrix.collidesWith(c1.getGameObject().getTag(),
 						c2.getGameObject().getTag()) &&
 						c1.getBounds().intersects(c2.getBounds())) {
-
 					collisions.get(c1).add(c2);
-					collisions.putIfAbsent(c2, new ArrayList<Collider>());
 					collisions.get(c2).add(c1);
-
-				}
+				}	
 			}
 			c1.updateCollisions(collisions.get(c1));
 		}
 	}
-
 }
